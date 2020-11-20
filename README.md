a6cb0437b573dddb6feb13e4bf2c6db36ac14926
PanSheng <21289700+iweb-wiki@users.noreply.github.com>
2020-11-20 23:45:15 +0800

    1.0.2 创建一个bundle

---
+++ README.md
@@ -0,0 +1,6 @@
+## 1.0.1
+- 基本安装
+## 1.0.2
+- 创建一个bundle
+- 添加dist目录
+- 使用 `npx webpack` 执行（transpile）转译
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+// import _ from "lodash";
+
 function component () {
     let ele = document.createElement('div');
-    ele.innerHTML = 'Hello webpack !'
+    ele.innerHTML = ['Hello', 'webpack', '!'].join(' ')
+    // ele.innerHTML = _.join(['Hello', 'webpack', '!'], ' ')
     return ele;
 }
 
