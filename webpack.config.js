80979a5fe0f570ab42bc8ee233fcb9155e2f229e
PanSheng <21289700+iweb-wiki@users.noreply.github.com>
2021-01-13 01:34:27 +0800

    loader 的基本使用

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
 const path = require("path");
 
+/**
+ * @type { import ('webpack').Configuration }
+ */
 module.exports = {
-  entry: {
-    app: './src/index.js',
-    print: './src/print.js'
-  },
+  mode: 'none',
+  entry: './src/index.js',
+  // entry: {
+  //   app: './src/index.js',
+  //   print: './src/print.js'
+  // },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js'
+    // filename: "[name].bundle.js",
+    // path: path.resolve(__dirname, "dist"),
   },
+  module:{
+    rules:[
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 };
